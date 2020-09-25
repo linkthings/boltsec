a5651816a7e73aec1e824003c96d1dc19c1ed909
SSH DOC <sshdoc@fastmail.com>
2020-09-25 08:51:18 +0700

    Removed JSON.Marshal on Boltsec.Save

--- README.md
+++ README.md
@@ -1,74 +1,83 @@
 # boltsec
-A boltdb wrapper to encrypt and decrypt the values stored in the boltdb via AES Cryptor, and also provides common 
-db operations such as `GetOne`, `GetByPrefix`, `GetKeyList`, `Save`, `Delete`, etc... 
 
-Boltdb file is always open in the file system unless  `DB.Close()` is called, which cause inconvenience 
-if you want to do some file operations to the db file while the program is running. This package provides the parameter: `batchMode` to 
-control whether to close the db after each db operation, this has performance impact but could be a useful feature.
+A boltdb wrapper to encrypt and decrypt the values stored in the boltdb via AES
+Cryptor, and also provides common db operations such as `GetOne`, `GetByPrefix`,
+`GetKeyList`, `Save`, `Delete`, etc...
+
+Boltdb file is always open in the file system unless  `DB.Close()` is called,
+which cause inconvenience if you want to do some file operations to the db file
+while the program is running. This package provides the parameter: `batchMode` to
+control whether to close the db after each db operation, this has performance
+impact but could be a useful feature.
 
 ## Features
+
 1. [x] Support AES to encrypt and decrypt the values
-1. [x] Batch mode option to control whether to close the db after each db operation 
+1. [x] Batch mode option to control whether to close the db after each operation
 1. [x] Initialize db file and cryptor
 
 ## Performance
+
 The below is the benchmark data for the DB related operations.
+
 ```golang
-BenchmarkDBMOps-8               	    2000	   1114202 ns/op
-BenchmarkDBMOpsBatchMode-8      	    5000	    383995 ns/op
-BenchmarkDBMOpsNoEncryption-8   	    1000	   1029510 ns/op
+BenchmarkDBMOps-8                       2000        1114202 ns/op
+BenchmarkDBMOpsBatchMode-8              5000        383995 ns/op
+BenchmarkDBMOpsNoEncryption-8           1000        1029510 ns/op
 ```
 
 ## LICENSE
+
 This project is under license [MIT](LICENSE)
 
 ## Usage Example
 
 <details>
-	<summary>header code</summary>
+    <summary>header code</summary>
 
 ```golang
 package main
 
 import (
-	"encoding/json"
-	"fmt"
+    "encoding/json"
+    "fmt"
 
-	"github.com/linkthings/boltsec"
+    "github.com/linkthings/boltsec"
 )
 
 type article struct {
-	ID            string
-	Name          string
+    ID            string
+    Name          string
 }
 ```
 
 </details>
 
-```golang	bucketName := "article"
+```golang   bucketName := "article"
 func main() {
-	bucketName := "article"
-	dbm, _ := boltsec.NewDBManager("test.dat", "", "secret", false, []string{bucketName})
+    bucketName := "article"
+    dbm, _ := boltsec.NewDBManager("test.dat", "", "secret", false, []string{bucketName})
 
-	data := article{
-		ID:   "ID-0001",
-		Name: "input with more than 16 characters",
-	}
+    data := article{
+        ID:   "ID-0001",
+        Name: "input with more than 16 characters",
+    }
 
-	dbm.Save(bucketName, data.ID, data)
+    dbm.Save(bucketName, data.ID, data)
 
-	var bytes []byte
-	bytes, _ = dbm.GetOne(bucketName, data.ID)
+    var bytes []byte
+    bytes, _ = dbm.GetOne(bucketName, data.ID)
 
-	resNew := new(article)
-	json.Unmarshal(bytes, resNew)
+    resNew := new(article)
+    json.Unmarshal(bytes, resNew)
 
-	if resNew.Name == data.Name {
-		fmt.Printf("Decrypted data matches origional input:\nDecrypted:%s\nOriginal:%s\n", resNew.Name, data.Name)
-	}
+    if resNew.Name == data.Name {
+        fmt.Printf("Decrypted data matches origional input:\nDecrypted:%s\nOriginal:%s\n", resNew.Name, data.Name)
+    }
 
-	dbm.Delete(bucketName, data.ID)
+    dbm.Delete(bucketName, data.ID)
 }
 ```
 
-A complete example with error handling and testing can be found in the [example](example/) folder
+A complete example with error handling and testing can be found in 
+the [example](example/) folder
--- boltsec_test.go
+++ boltsec_test.go
@@ -3,6 +3,7 @@ package boltsec
 import (
 	"encoding/json"
 	"testing"
+	proto "github.com/golang/protobuf/proto"
 )
 
 type Article struct {
@@ -31,40 +32,60 @@ func ExampleNewDBManager() {
 	}
 }
 
-func TestDBMCreate(t *testing.T) {
-	var err error
-	bucketName := "article"
+func TestJsonCreate(t *testing.T) {
+	// marshal := func (input interface{}) (out interface{}) {
+	// 	value, err := json.Marshal(input)
+	// 	if err != nil {
+	// 		t.Errorf()
+	// 	}
+	// 	return value
+	// }
 
-	dbm, err := NewDBManager("test.dat", "example", "secret", false, []string{bucketName})
+	bucketName := "jsonArticle"
+	dbm, err := NewDBManager("json.dat", ".", "secret", false, []string{bucketName})
 
 	data := Article{
-		ID:    "ID-0001",
-		Title: "input with more than 16 characters",
+		ID:    "JSON-0001",
+		Title: "How much JSON could a jchuck chuck if a jchuck could chuck json?",
+	}
+	rawData, err := json.Marshal(data)
+	if err != nil {
+		t.Errorf("Json Serialization Failed: %s", err)
 	}
 
-	if err = dbm.Save(bucketName, data.ID, data); err != nil {
-		t.Errorf("TestDBMCreate save data return err: %s", err)
+	if err = dbm.Save(bucketName, data.ID, rawData); err != nil {
+		t.Errorf("TestJsonCreate save data returned: %s", err)
+	}
+
+}
+
+func TestProtobufCreate(t *testing.T) {
+	bucketName := "protobufArticle"
+	dbm, err := NewDBManager("protobuf.data", ".", "secret", false, []string{bucketName})
+
+	data := &ProtobufArticle{
+		ID:		"PROTOBUF-0001",
+		Title:  "We have a protobuf here...",
+	}
+
+	rawData, err := proto.Marshal(data)
+	if err = dbm.Save(bucketName, data.ID, rawData); err != nil {
+		t.Errorf("TestProtobufCreate save data returned: %s", err)
 	}
 
 	var bytes []byte
 	if bytes, err = dbm.GetOne(bucketName, data.ID); err != nil {
-		t.Errorf("TestDBMCreate GetOne return err: %s", err)
+		t.Errorf("dbm.GetOne error: %s", err)
 	}
 
-	resNew := new(Article)
-	if err = json.Unmarshal(bytes, resNew); err != nil {
-		t.Errorf("json.Unmarshal return err: %s", err)
+	data2 := &ProtobufArticle{}
+	if err = proto.Unmarshal(bytes, data2); err != nil {
+		t.Errorf("protobuf unmarshal error: %s", err)
 	}
 
-	if resNew.Title != data.Title {
-		t.Errorf("returned Title is not equal: new:%s, org: %s", resNew.Title, data.Title)
+	if ! proto.Equal(data, data2) {
+		t.Errorf("Marshal/Save/Get/Unmarshal != source data")
 	}
-
-	if err = dbm.Delete(bucketName, data.ID); err != nil {
-		t.Errorf("TestDBMCreate Delete return err: %s", err)
-	}
-
-	return
 }
 
 func BenchmarkDBMOps(b *testing.B) {
@@ -78,8 +99,13 @@ func BenchmarkDBMOps(b *testing.B) {
 		Title: "input with more than 16 characters",
 	}
 
+	rawData, err := json.Marshal(data)
+	if err != nil {
+		b.Errorf("Benchmark Error - Marshaling Json...: %s", err)
+	}
+
 	for n := 0; n < b.N; n++ {
-		if err = dbm.Save(bucketName, data.ID, data); err != nil {
+		if err = dbm.Save(bucketName, data.ID, rawData); err != nil {
 			b.Errorf("TestDBMCreate save data return err: %s", err)
 		}
 
@@ -113,9 +139,13 @@ func BenchmarkDBMOpsBatchMode(b *testing.B) {
 		ID:    "ID-0001",
 		Title: "input with more than 16 characters",
 	}
+	rawData, err := json.Marshal(data)
+	if err != nil {
+		b.Errorf("Benchmark Error - Marshaling Json...: %s", err)
+	}
 
 	for n := 0; n < b.N; n++ {
-		if err = dbm.Save(bucketName, data.ID, data); err != nil {
+		if err = dbm.Save(bucketName, data.ID, rawData); err != nil {
 			b.Errorf("TestDBMCreate save data return err: %s", err)
 		}
 
@@ -151,9 +181,13 @@ func BenchmarkDBMOpsNoEncryption(b *testing.B) {
 		ID:    "ID-0001",
 		Title: "input with more than 16 characters",
 	}
+	rawData, err := json.Marshal(data)
+	if err != nil {
+		b.Errorf("Benchmark Error - Marshaling Json...: %s", err)
+	}
 
 	for n := 0; n < b.N; n++ {
-		if err = dbm.Save(bucketName, data.ID, data); err != nil {
+		if err = dbm.Save(bucketName, data.ID, rawData); err != nil {
 			b.Errorf("TestDBMCreate save data return err: %s", err)
 		}
 
